44f34722b1e48f6010b38a23b8e7af8525034ca4
wangjiang <ji.ang@outlook.com>
2022-08-25 10:55:29 +0800

    update data insert sql

--- CSE511_Assignment1_Verification.sql
+++ CSE511_Assignment1_Verification.sql
@@ -1,14 +1,14 @@
 -- TEST CASE 1: Insert normal data
 -- INSERT INTO users VALUES (85281, 'testName');
 -- INSERT INTO movies VALUES (20030628, 'Pirates of the Caribbean: The Curse of the Black Pearl (2003)');
--- INSERT INTO taginfo VALUES (1234, 'pirates');
+-- INSERT INTO taginfo VALUES (3280, 'pirates');
 -- INSERT INTO genres VALUES (5678, 'adventure');
 -- INSERT INTO ratings VALUES (85281, 20030628, 4.5, 1661228069);
 -- INSERT INTO tags VALUES (85281, 20030628, 1234, 1661228069);
 -- INSERT INTO hasagenre VALUES (20030628, 5678);
 
 -- TEST CASE 2: Insert non-exist foreign key
-INSERT INTO ratings VALUES (100000, 1, 0, 1661228069);
+-- INSERT INTO ratings VALUES (1000000, 1, 0, 1661228069);
 
 -- TEST CASE 3: Insert duplicate rating
 -- INSERT INTO ratings VALUES (85281, 20030628, 5, 1661228069);
@@ -17,5 +17,5 @@ INSERT INTO ratings VALUES (100000, 1, 0, 1661228069);
 -- INSERT INTO hasagenre VALUES (20030628, 6789);
 
 -- TEST CASE 5: Insert a rating larger than 5
--- INSERT INTO movies VALUES (20060624, 'Pirates of the Caribbean: Dead Man''s Chest (2006)');
--- INSERT INTO ratings VALUES (85281, 20060624, 6, 1661228069);
+INSERT INTO movies VALUES (20060624, 'Pirates of the Caribbean: Dead Man''s Chest (2006)');
+INSERT INTO ratings VALUES (85281, 20060624, 6, 1661228069);
--- add_data.sql
+++ add_data.sql
@@ -1,11 +1,12 @@
 
 
 -- import data from .dat files
+-- docker cp "D:\docker\cse511\exampleinput\." postgres-MBCm:/home/dat
 
-COPY users FROM '/home/users.dat' DELIMITER '%';
-COPY movies FROM '/home/movies.dat' DELIMITER '%';
-COPY taginfo FROM '/home/taginfo.dat' DELIMITER '%';
-COPY genres FROM '/home/genres.dat' DELIMITER '%';
-COPY ratings FROM '/home/ratings.dat' DELIMITER '%';
-COPY tags FROM '/home/tags.dat' DELIMITER '%';
-COPY hasagenre FROM '/home/hasagenre.dat' DELIMITER '%';
+COPY users FROM '/home/dat/users.dat' DELIMITER '%';
+COPY movies FROM '/home/dat/movies.dat' DELIMITER '%';
+COPY taginfo FROM '/home/dat/taginfo.dat' DELIMITER '%';
+COPY genres FROM '/home/dat/genres.dat' DELIMITER '%';
+COPY ratings FROM '/home/dat/ratings.dat' DELIMITER '%';
+COPY tags FROM '/home/dat/tags.dat' DELIMITER '%';
+COPY hasagenre FROM '/home/dat/hasagenre.dat' DELIMITER '%';
