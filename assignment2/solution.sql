ea9b249e7b06948d3a4d29336b4a4170975c7b8e
wangjiang <ji.ang@outlook.com>
2022-09-02 18:22:53 +0800

    for assignment2

---
+++ assignment2/solution.sql
@@ -0,0 +1,13 @@
+-- CLEAR TABLE
+-- DROP TABLE query1;
+-- DROP TABLE query2;
+-- DROP TABLE query3;
+-- DROP TABLE query4;
+-- DROP TABLE query5;
+-- DROP TABLE query6;
+-- DROP TABLE query7;
+-- DROP TABLE query8;
+-- DROP TABLE query9;
+
+-- query 1
+SELECT name, moviecount
